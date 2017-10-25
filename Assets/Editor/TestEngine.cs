82ea3d0efdd0ec4c70da014429dcdc5f22068e37
jakubbadacz <b00080699@student.itb.ie>
2017-10-25 11:53:48 +0100

    Adding Shield files and renaming files

---
+++ Assets/Editor/TestEngine.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class TestEngine
+{
+
+	[Test]
+	public void TestTryToTurnEngineOnWhenOn()
+    {
+        Engine engine = new Engine();
+        engine.SetIsOn(true);
+        bool expectedResult = true;
+        Assert.AreEqual(expectedResult, engine.GetIsOn());
+	}
+
+    [Test]
+    public void TestTryToTurnEngineOffWhenOff()
+    {
+        Engine engine = new Engine();
+        engine.SetIsOn(false);
+        bool expectedResult = false;
+        Assert.AreEqual(expectedResult, engine.GetIsOn());
+    }
+
+    [Test]
+    public void TestAccelerateWhenEngineOff()
+    {
+        Engine engine = new Engine();
+        engine.SetIsOn(false);
+		bool falseblat = false;
+        bool expectedResult = false;
+		Assert.AreEqual(expectedResult, falseblat);
+    }
+}
--- Assets/SBSP-A1-nav-combat/Thruster/Scripts/ThrusterView.cs
+++ Assets/SBSP-A1-nav-combat/Thruster/Scripts/ThrusterView.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestShipTruster : MonoBehaviour {
+public class ThrusterView : MonoBehaviour {
 
 	public ThrusterController fl, fr, rl, rr;
 
