5da2580228e80c0949a8d63c9b158193146a0c16
jakubbadacz <b00080699@student.itb.ie>
2017-11-15 14:04:46 +0000

    Adding Engine UI Constraints

--- Assets/SBSP-A1-nav-combat/Engine/Scripts/EngineView.cs
+++ Assets/SBSP-A1-nav-combat/Engine/Scripts/EngineView.cs
@@ -6,55 +6,55 @@ using UnityEngine.UI;
 public class EngineView : MonoBehaviour {
 
 
-    public Text powerText;
-    public Text engineStateText;
-    public Button onEngineButton;
-    public Button offEngineButton;
-    public Slider accSlider;
+	public Text powerText;
+	public Text engineStateText;
+	public Button onEngineButton;
+	public Button offEngineButton;
+	public Slider accSlider;
 
-    private EngineController engineCont;
+	private EngineController engineCont;
 
 
-    private void Awake()
-    {
-        engineCont = GetComponent<EngineController>();
-    }
+	private void Awake()
+	{
+		engineCont = GetComponent<EngineController>();
+	}
 
-    private void Start()
-    {
+	private void Start()
+	{
 
-        onEngineButton.onClick.AddListener(engineCont.TurnOnEngine);
-        offEngineButton.onClick.AddListener(engineCont.TurnOffEngine);
-        accSlider.onValueChanged.AddListener(delegate { engineCont.Accelerate(accSlider); });
-    }
+		onEngineButton.onClick.AddListener(engineCont.TurnOnEngine);
+		offEngineButton.onClick.AddListener(engineCont.TurnOffEngine);
+		accSlider.onValueChanged.AddListener(delegate { engineCont.Accelerate(accSlider); });
+	}
 
-    //update text message for the power value
-    public void UpdatePower(float power)
-    {
-        string powerMessage = "Power : " + power;
+	//update text message for the power value
+	public void UpdatePower(float power)
+	{
+		string powerMessage = "Power : " + power;
 
-        //update UI on the screen with the message
-        powerText.text = powerMessage;
+		//update UI on the screen with the message
+		powerText.text = powerMessage;
 
-    }
+	}
 
-    public void UpdateEngineState(bool engineState)
-    {
-        string engineStateString;
+	public void UpdateEngineState(bool engineState)
+	{
+		string engineStateString;
 
-        if (engineState)
-        {
-            engineStateString = "ON";
-        }
-        else
-        {
-            engineStateString = "OFF";
-            powerText.text = "Power : 0";
-        }
+		if (engineState)
+		{
+			engineStateString = "ON";
+		}
+		else
+		{
+			engineStateString = "OFF";
+			powerText.text = "Power : 0";
+		}
 
-        string engineStateMessage = "Engine : " + engineStateString;
+		string engineStateMessage = "Engine : " + engineStateString;
 
-        engineStateText.text = engineStateMessage;
-    }
+		engineStateText.text = engineStateMessage;
+	}
 
 }
