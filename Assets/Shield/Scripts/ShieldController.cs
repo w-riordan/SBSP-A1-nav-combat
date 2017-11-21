a4b927ebc70e10b53032d83d1128b8e8a7c4363b
dcushen <cushen.dave@gmail.com>
2017-11-21 16:47:02 +0000

    Enemy Kamikaze Ship, shield change, thermal rocket
    Enemy Kamikaze Ship 
    - A sphere object, when within a certain distance, it will fly towards game object with tag 'ship' (our ship), if it hits, it will destroy your ship 
    
    Because there was no counterplay, created a thermal rocket
    - Press B after dragging in ThermalRocketLauncher prefab, this will spawn a cylinder, which will fly towards game object with tag 'Enemy Kamikaze Ship' when within 500 distance of the ship. Will destroy on collision.

---
+++ Assets/Shield/Scripts/ShieldController.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class ShieldController : MonoBehaviour
+{
+
+    public ShieldModel shield;
+    private ShieldView sView;
+
+    void Start()
+    {
+        //Create new ShieldModel object
+        shield = new ShieldModel();
+        sView = GetComponent<ShieldView>();
+        sView.UpdateShieldStatus(shield.GetIsOn());
+        sView.UpdateShieldHealth(shield.GetShieldHealth(), shield.GetShieldStrength());
+        sView.UpdateShieldStrength(shield.GetShieldStrength());
+        StartCoroutine(RegenShield());
+        
+    }
+
+    private void Update()
+    {
+        sView.UpdateShieldHealth(shield.GetShieldHealth(), shield.GetShieldStrength());
+        
+    }
+
+    public void ChangeStrength(Slider StrengthSlider) // changing shield strength with slider
+    {
+        if(shield.isOn)
+        {
+            shield.SetShieldStrength((StrengthSlider.value));
+            sView.UpdateShieldStrength(shield.GetShieldStrength());
+            StrengthSlider.interactable = true;
+        }
+        else
+        {
+            StrengthSlider.interactable = false; // can't move slider if shield inactive
+            Debug.Log("Shield is not on, can't change power.");
+        }
+    }
+
+    public void TurnOnShield() // turn shield on 
+    {
+        if(!shield.GetIsOn())
+        {
+            Debug.Log("Shield turned on");
+            shield.SetIsOn(true);
+            sView.UpdateShieldStatus(shield.GetIsOn());
+           
+        }
+        else
+        {
+            Debug.Log("Shield already on!");
+        }
+    }
+    public void TurnOffShield() // turn shield off
+    {
+        if (shield.GetIsOn())
+        {
+            Debug.Log("Shield turned off");
+            shield.SetIsOn(false); // set shield bool to false
+            sView.UpdateShieldStatus(shield.GetIsOn()); // update
+            shield.SetShieldStrength(0); // remove shield strength, set to 0 
+            
+        }
+        else
+        {
+            Debug.Log("Shield if already off");
+        }
+
+    }
+
+    IEnumerator RegenShield()
+    {
+        while(true) // do forever while shield health less than 100
+        {
+            if(shield.ShieldHealth < 500) // if shield health is less than 100
+            {
+                shield.ShieldHealth += 1; // increase shield health
+                yield return new WaitForSeconds(1); // wait one second, regen again
+            } else // if it is more than 500 just return null 
+            {
+                yield return null;
+            }
+        }
+    }
+    void OnCollisionEnter(Collision col) // on collision, depending on tag type, apply damage to health
+    {
+        if(col.gameObject.tag == "Cannon")
+        {
+            shield.ShieldHealth = shield.ShieldHealth - 100; 
+            Debug.Log("Hit by a cannon. -100 health.");
+            shield.SetWasDamaged(true);
+        }
+        if(col.gameObject.tag == "Missile")
+        {
+            shield.ShieldHealth = shield.ShieldHealth - 150;
+            Debug.Log("Hit by a missile. -150 health");
+            shield.SetWasDamaged(true);
+        }
+        if (col.gameObject.tag == "enemyShip")
+        {
+            Debug.Log("Enemy ship collided. -500 health");
+        }
+
+
+    }
+}
---
+++ Assets/Shield/Scripts/ShieldModel.cs
@@ -0,0 +1,48 @@
+﻿public class ShieldModel
+{
+    // declare vars
+    public float ShieldHealth = 500;
+    public float ShieldStrength = 0;
+    public bool isOn = false;
+    public bool WasDamaged = false;
+
+    public void SetShieldHealth(int ShieldHealth)
+    {
+        this.ShieldHealth = ShieldHealth;
+    }
+
+    public float GetShieldHealth()
+    {
+        return this.ShieldHealth;
+    }
+
+    public void SetShieldStrength(float ShieldStrength)
+    {
+        this.ShieldStrength = ShieldStrength;
+    }
+
+    public float GetShieldStrength()
+    {
+        return this.ShieldStrength;
+    }
+
+    public bool GetIsOn()
+    {
+        return this.isOn;
+    }
+
+    public void SetIsOn(bool isOn)
+    {
+        this.isOn = isOn;
+    }
+
+    public void SetWasDamaged(bool WasDamaged)
+    {
+        this.WasDamaged = WasDamaged;
+    }
+
+    public bool GetWasDamaged()
+    {
+        return this.WasDamaged;
+    }
+}
