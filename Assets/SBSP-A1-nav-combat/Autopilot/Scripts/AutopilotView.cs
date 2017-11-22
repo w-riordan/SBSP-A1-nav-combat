7453d89be25e66a57ea42273992e6e32109abe1e
Wayne <wayneriordan@gmail.com>
2017-11-22 16:23:36 +0000

    Updated Auto pilot and UI for auto pilot

--- Assets/SBSP-A1-nav-combat/Autopilot/Scripts/AutopilotView.cs
+++ Assets/SBSP-A1-nav-combat/Autopilot/Scripts/AutopilotView.cs
@@ -7,12 +7,11 @@ public class AutopilotView : MonoBehaviour {
 
 	public Toggle autopilotToggle;
 	public Slider headingSlider;
-	public Text headingText;
+	public Text targetHeadingText,currentHeading;
 	private AutopilotController apCon;
 	// Use this for initialization
 	void Start () {
 		apCon = GetComponent<AutopilotController> ();
-
 
 		autopilotToggle.onValueChanged.AddListener (delegate {
 			apCon.SetAutopilot(autopilotToggle.isOn);
@@ -23,8 +22,13 @@ public class AutopilotView : MonoBehaviour {
 
 	}
 
+	void Update() {
+		currentHeading.text = "Current Heading: " + Mathf.RoundToInt(apCon.GetCurrentHeading());
+	}
+
 	public void UpdateTargetHeading(float heading){
 		apCon.SetTargetHeading (heading);
-		headingText.text = "Heading: "+heading;
+		heading = (heading < 0) ? heading + 360 : heading;
+				targetHeadingText.text = "Target Heading: " + Mathf.RoundToInt(heading);
 	}
 }
